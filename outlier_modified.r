00fdc61d4b6bfed4e5d5efc43eee7130cc1fbc87
Omar Santos <santosomar@gmail.com>
2018-02-11 18:38:46 -0500

    Create outlier_modified.r

---
+++ outlier_modified.r
@@ -0,0 +1,22 @@
+outlierKD <- function(var) {
+     var_name = var
+     na1 <- sum(is.na(var_name))
+     m1 <- mean(var_name, na.rm = T)
+     par(mfrow=c(2, 2), oma=c(0,0,3,0))
+     boxplot(var_name, main="With outliers")
+     hist(var_name, main="With outliers", xlab=NA, ylab=NA)
+     outlier <- boxplot.stats(var_name)$out
+     mo <- mean(outlier)
+     var_name <- ifelse(var_name %in% outlier, NA, var_name)
+     boxplot(var_name, main="Without outliers")
+     hist(var_name, main="Without outliers", xlab=NA, ylab=NA)
+     title("Outlier Check", outer=TRUE)
+     na2 <- sum(is.na(var_name))
+     cat("Outliers identified:", na2 - na1, "\n")
+     cat("Propotion (%) of outliers:", round((na2 - na1) / sum(!is.na(var_name))*100, 1), "\n")
+     cat("Mean of the outliers:", round(mo, 2), "\n")
+     m2 <- mean(var_name, na.rm = T)
+     cat("Mean without removing outliers:", round(m1, 2), "\n")
+     cat("Mean if we remove outliers:", round(m2, 2), "\n")
+     
+}
